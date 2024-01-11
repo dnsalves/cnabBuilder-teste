0133a9201d70f000f00711bf2a6fe978c3e36246
Dominic Monteiro <monteiro2silva@gmail.com>
2024-01-11 11:38:49 -0300

    Add initial files and folders

---
+++ cnabRows.js
@@ -0,0 +1,115 @@
+'use strict';
+import path from 'path'
+import { readFile } from 'fs/promises'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+import yargs from 'yargs'
+import chalk from 'chalk'
+
+const optionsYargs = yargs(process.argv.slice(2))
+  .usage('Uso: $0 [options]')
+  .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
+  .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
+  .option("s", { alias: "segment", describe: "tipo de segmento", type: "string" })
+  .option("n", { alias: "companyName", describe: "nome da empresa de pesquisa da linha do Cnab", type: "string" })
+  .option("a", { alias: "archive", describe: "caminho do arquivo Cnab", type: "string", demandOption: true })
+  .check(argv => {
+    if (!argv.companyName && !argv.segment) {
+      throw new Error('A opção "segment" é obrigatória se "companyName" não for fornecido.');
+    }
+    return true;
+  })
+  .example('$0 -f 21 -t 34 -s p -n "NomeEmpresa" -a ./caminho/do/seu/arquivo.cnab', 'lista a linha e campo que from e to do cnab')
+  .argv;
+
+const { from, to, segment, companyName, archive = '' } = optionsYargs
+
+const filePath = path.isAbsolute(archive) ? archive : path.resolve(fileURLToPath(import.meta.url), archive)
+
+const sliceArrayPosition = (arr, ...positions) => [...arr].slice(...positions)
+
+const messageLog = (segment, segmentType, from, to) => `
+----- Cnab linha ${segmentType} -----
+
+posição from: ${chalk.inverse.bgBlack(from)}
+
+posição to: ${chalk.inverse.bgBlack(to)}
+
+item isolado: ${chalk.inverse.bgBlack(segment.substring(from - 1, to))}
+
+item dentro da linha ${segmentType.toUpperCase()}: 
+  ${segment.substring(0, from)}${chalk.inverse.bgBlack(segment.substring(from - 1, to))}${segment.substring(to)}
+
+----- FIM ------
+`
+
+const log = console.log
+
+console.time('leitura Async')
+
+readFile(filePath, 'utf8')
+  .then(file => {
+    const cnabArray = file.split('\n')
+
+    // const cnabHeader = sliceArrayPosition(cnabArray, 0, 2)
+
+    const cnabBodySegments = sliceArrayPosition(cnabArray, 2, -2)
+    const [cnabBodySegmentP, cnabBodySegmentQ, cnabBodySegmentR] = cnabBodySegments
+
+    // const cnabTail = sliceArrayPosition(cnabArray, -2)
+
+    if (companyName) {
+      const companyNameStart = 33;
+      const companyNameEnd = 72;
+      const companyAddressStart = 72;
+      const companyAddressEnd = 127;
+
+      const companyNameRegex = new RegExp(companyName, 'i');
+      const companies = [];
+
+      cnabBodySegments.forEach((segment) => {
+        const match = segment.match(companyNameRegex);
+
+        if (match) {
+          const matchStartPosition = match.index + 1;
+          const matchEndPosition = matchStartPosition + match[0].length - 1;
+
+          const segmentInitialCode = segment.split(' ')[0];
+          const segmentType = segmentInitialCode[segmentInitialCode.length - 1];
+
+          log(messageLog(segment, segmentType, matchStartPosition, matchEndPosition));
+
+          const companyName = segment.substring(companyNameStart, companyNameEnd).trim().replace(/\s+/g, ' ');
+          const companyAddress = segment.substring(companyAddressStart, companyAddressEnd).trim().replace(/\s+/g, ' ');
+
+          companies.push({ name: companyName, address: companyAddress });
+
+          const jsonCompanies = JSON.stringify(companies, null, 2);
+
+          const jsonFilePath = path.join(new URL('.', import.meta.url).pathname, 'companies.json');
+
+          fs.writeFileSync(jsonFilePath, jsonCompanies, 'utf-8');
+        }
+      });
+    } else {
+      if (segment === 'p') {
+        log(messageLog(cnabBodySegmentP, 'P', from, to))
+        return
+      }
+  
+      if (segment === 'q') {
+        log(messageLog(cnabBodySegmentQ, 'Q', from, to))
+        return
+      }
+  
+      if (segment === 'r') {
+        log(messageLog(cnabBodySegmentR, 'R', from, to))
+        return
+      }
+    }
+  })
+  .catch(error => {
+    console.log("🚀 ~ file: cnabRows.js ~ line 76 ~ error", error)
+  })
+console.timeEnd('leitura Async')
